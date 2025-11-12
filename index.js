58c7827ae52a783362cf04dcf1f5a93493181a18
Piyush Bhardwaj <166249777+PIyYshBHardwaJ@users.noreply.github.com>
2025-11-12 08:48:46 +0000

    Switch to EmailJS send-private endpoint

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post("/send-report", async (req, res) => {
   console.log(req.body);
 
   try {
-    const response = await fetch("https://api.emailjs.com/api/v1.0/email/send", {
+    const response = await fetch("https://api.emailjs.com/api/v1.0/email/send-private", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
