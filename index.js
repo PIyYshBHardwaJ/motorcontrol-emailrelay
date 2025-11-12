917a6430cdebb5550d21b39091e26970ed26062b
Piyush Bhardwaj <166249777+PIyYshBHardwaJ@users.noreply.github.com>
2025-11-12 19:32:14 +0000

    Added patient email to email body and recipients

--- index.js
+++ index.js
@@ -1,51 +1,4 @@
-// import express from "express";
-// import fetch from "node-fetch";
 
-// const app = express();
-// app.use(express.json());
-
-// const RESEND_API_KEY = "re_GD94N3Fn_DTpASLuydz2SRhZvC4U9qNag";
-// const RECEIVER_EMAIL = "piyushbhardwaj418@gmail.com";
-// const SENDER_EMAIL = "onboarding@resend.dev"; // any valid sender
-
-// app.post("/send-report", async (req, res) => {
-//   const { name, email, date, time, clockwise, anticlockwise, total } = req.body;
-
-//   console.log("📨 Report received from ESP32:", req.body);
-
-//   const emailBody = `
-//     <h2>Motor Control Session Summary</h2>
-//     <p><b>Patient:</b> ${name}</p>
-//     <p><b>Patient Email:</b> ${email}</p
-//     <p><b>Date:</b> ${date}</p>
-//     <p><b>Time:</b> ${time}</p>
-//     <p><b>clockwise:</b> ${clockwise}</p>
-//     <p><b>anticlockwise:</b> ${anticlockwise}</p>
-//     <p><b>Total:</b> ${total} seconds</p>
-//   `;
-
-//   // 🚀 Send via Resend API
-//   const response = await fetch("https://api.resend.com/emails", {
-//     method: "POST",
-//     headers: {
-//       "Authorization": `Bearer ${RESEND_API_KEY}`,
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       from: SENDER_EMAIL,
-//       to: [RECEIVER_EMAIL],
-//       subject: `Patient Report - ${name}`,
-//       html: emailBody,
-//     }),
-//   });
-
-//   const text = await response.text();
-//   console.log("✅ Resend response:", response.status, text);
-//   res.status(200).send("✅ Report received. Email sent via Resend.");
-// });
-
-// const PORT = process.env.PORT || 10000;
-// app.listen(PORT, () => console.log(`🚀 Resend Gmail relay running on port ${PORT}`));
 import express from "express";
 import fetch from "node-fetch";
 
@@ -54,7 +7,7 @@ app.use(express.json());
 
 // ✅ Replace these values accordingly
 const RESEND_API_KEY = "re_GD94N3Fn_DTpASLuydz2SRhZvC4U9qNag";
-const RECEIVER_EMAIL = "piyushbhardwaj418@gmail.com";
+const RECEIVER_EMAIL = "unhardik@gmail.com";
 const SENDER_EMAIL = "onboarding@resend.dev"; // any verified sender email
 
 // 📩 Route to receive ESP32 report and send email
